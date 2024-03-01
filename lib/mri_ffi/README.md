242e3621fe5f02de14dc67d841ec188cae4b3db6
Herwin <herwinw@users.noreply.github.com>
2024-03-01 17:13:24 +0100

    Copy-paste an example of a Ruby lib in C

---
+++ lib/mri_ffi/README.md
@@ -0,0 +1,5 @@
+To compile:
+
+   ruby extconf.rb
+   make
+   ruby call-methods-ruby.rb
---
+++ lib/mri_ffi/call-methods-ruby.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative 'methods'
+
+"Hello, world!".my_fixed_args_method(true, 1234)
