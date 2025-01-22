7ef6038fa0e3b347a4ede786437b46ff727c0a64
Herwin <herwinw@users.noreply.github.com>
2025-01-22 18:31:31 +0100

    Check input type of argument in define_finalizer
    Certain types cannot have a finalizer, including frozen objects (even
    though we could make it work).

--- src/object_space_module.cpp
+++ src/object_space_module.cpp
@@ -3,6 +3,10 @@
 namespace Natalie {
 
 ArrayObject *ObjectSpaceModule::define_finalizer(Env *env, Value obj, Value aProc, Block *block) {
+    if (obj.is_integer() || obj->is_float() || obj->is_nil() || obj->is_true() || obj->is_false())
+        env->raise("ArgumentError", "cannot define finalizer for {}", obj->klass()->inspect_str());
+    if (obj->is_frozen())
+        env->raise("FrozenError", "can't modify frozen {}: {}", obj->klass()->inspect_str(), obj->inspect_str(env));
     if (!aProc && !block)
         env->raise("ArgumentError", "tried to create Proc object without a block");
     static const auto call = "call"_s;
