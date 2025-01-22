6599633a694f4e99c2d69538acce8aca55b68bea
Herwin <herwinw@users.noreply.github.com>
2025-01-22 18:06:18 +0100

    Fix return value of ObjectSpace.define_finalizer

--- src/object_space_module.cpp
+++ src/object_space_module.cpp
@@ -8,7 +8,9 @@ ArrayObject *ObjectSpaceModule::define_finalizer(Env *env, Value obj, Value aPro
     static const auto call = "call"_s;
     if (aProc && !aProc->respond_to(env, call))
         env->raise("ArgumentError", "wrong type argument {} (should be callable)", aProc->klass()->inspect_str());
-    return new ArrayObject;
+    if (!aProc)
+        aProc = new ProcObject { block };
+    return new ArrayObject { Value::integer(0), aProc };
 }
 
 }
