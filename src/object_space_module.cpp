5177e1b72967b599b5b8675e027a961b11808729
Herwin <herwinw@users.noreply.github.com>
2025-01-22 13:50:53 +0100

    Validate arguments in define_finalizer

--- src/object_space_module.cpp
+++ src/object_space_module.cpp
@@ -3,6 +3,11 @@
 namespace Natalie {
 
 ArrayObject *ObjectSpaceModule::define_finalizer(Env *env, Value obj, Value aProc, Block *block) {
+    if (!aProc && !block)
+        env->raise("ArgumentError", "tried to create Proc object without a block");
+    static const auto call = "call"_s;
+    if (aProc && !aProc->respond_to(env, call))
+        env->raise("ArgumentError", "wrong type argument {} (should be callable)", aProc->klass()->inspect_str());
     return new ArrayObject;
 }
 
