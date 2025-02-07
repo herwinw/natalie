c11fd3490c3e877fa68ad90aab5321988a7da16c
Herwin <herwinw@users.noreply.github.com>
2025-02-07 13:30:44 +0100

    Experiment: replace zlib with ffi-zlib
    This does not yet work, but it's a nice spec for our FFI implementation.
    If we really merge this one, update the README as well.
    
    Source: https://github.com/lucsky/ffi-zlib/tree/master
    License: MIT

--- lib/zlib.rb
+++ lib/zlib.rb
@@ -1,189 +1,168 @@
-require 'natalie/inline'
-require 'zlib.cpp'
+# =============================================================================
 
-__ld_flags__ '-lz'
+require "ffi"
 
-module Zlib
-  __constant__ 'ASCII', 'int', 'Z_ASCII'
-  __constant__ 'BEST_COMPRESSION', 'int', 'Z_BEST_COMPRESSION'
-  __constant__ 'BEST_SPEED', 'int', 'Z_BEST_SPEED'
-  __constant__ 'BINARY', 'int', 'Z_BINARY'
-  __constant__ 'DEFAULT_COMPRESSION', 'int', 'Z_DEFAULT_COMPRESSION'
-  __constant__ 'DEFAULT_STRATEGY', 'int', 'Z_DEFAULT_STRATEGY'
-  DEF_MEM_LEVEL = 8 # Not defined in the zlib source, value copied from MRI documentation
-  __constant__ 'FILTERED', 'int', 'Z_FILTERED'
-  __constant__ 'FINISH', 'int', 'Z_FINISH'
-  __constant__ 'FIXED', 'int', 'Z_FIXED'
-  __constant__ 'HUFFMAN_ONLY', 'int', 'Z_HUFFMAN_ONLY'
-  __constant__ 'MAX_MEM_LEVEL', 'int', 'MAX_MEM_LEVEL'
-  __constant__ 'MAX_WBITS', 'int', 'MAX_WBITS'
-  __constant__ 'NO_COMPRESSION', 'int', 'Z_NO_COMPRESSION'
-  __constant__ 'RLE', 'int', 'Z_RLE'
-  __constant__ 'UNKNOWN', 'int', 'Z_UNKNOWN'
+# =============================================================================
 
-  class Error < StandardError; end
-  class BufError < Error; end
-  class DataError < Error; end
-  class MemError < Error; end
-  class NeedDict < Error; end
-  class StreamEnd < Error; end
-  class StreamError < Error; end
-  class VersionError < Error; end
+module FFI::Zlib
+    extend FFI::Library
+    ffi_lib("z")
 
-  def self._error(num)
-    case num
-    when 1
-      raise StreamEnd
-    when 2
-      raise NeedDict
-    when -1
-      __inline__ "env->raise_errno();"
-    when -2
-      raise StreamError
-    when -3
-      raise DataError
-    when -4
-      raise MemError
-    when -5
-      raise BufError
-    when -6
-      raise VersionError
-    else
-      raise Error, "unknown error"
-    end
-  end
+    # -------------------------------------------------------------------------
 
-  #class << self
-  __bind_static_method__ :adler32, :Zlib_adler32
-  __bind_static_method__ :crc32, :Zlib_crc32
-  __bind_static_method__ :crc_table, :Zlib_crc_table, 0
-  __bind_static_method__ :zlib_version, :Zlib_zlib_version, 0
-  #end
+    attach_function :zlibVersion, [ ], :string
 
-  def self.deflate(...)
-    Deflate.deflate(...)
-  end
+    # -------------------------------------------------------------------------
 
-  def self.inflate(...)
-    Inflate.inflate(...)
-  end
+    ZLIB_VERSION            = zlibVersion()
 
-  def self.gunzip(input)
-    # windowBits can also be greater than 15 for optional gzip encoding. Add
-    # 16 to windowBits to write a simple gzip header and trailer around the
-    # compressed data instead of a zlib wrapper.
-    zstream = Zlib::Inflate.new(MAX_WBITS | 16)
-    zstream << input
-    zstream.finish.tap do
-      zstream.close
-    end
-  end
+    # Allowed flush values.
 
-  def self.gzip(src, level: nil, strategy: nil)
-    # windowBits can also be greater than 15 for optional gzip encoding. Add
-    # 16 to windowBits to write a simple gzip header and trailer around the
-    # compressed data instead of a zlib wrapper.
-    zstream = Zlib::Deflate.new(level || DEFAULT_COMPRESSION, MAX_WBITS | 16, DEF_MEM_LEVEL, strategy || DEFAULT_STRATEGY)
-    zstream << src
-    zstream.finish.tap do
-      zstream.close
-    end
-  end
+    Z_NO_FLUSH              = 0
+    Z_PARTIAL_FLUSH         = 1
+    Z_SYNC_FLUSH            = 2
+    Z_FULL_FLUSH            = 3
+    Z_FINISH                = 4
+    Z_BLOCK                 = 5
 
-  class ZStream
-    __bind_method__ :adler, :Zlib_ZStream_adler, 0
-    __bind_method__ :avail_in, :Zlib_ZStream_avail_in, 0
-    __bind_method__ :avail_out, :Zlib_ZStream_avail_out, 0
-    __bind_method__ :data_type, :Zlib_ZStream_data_type, 0
+    # Return codes for the compression/decompression functions.
+    # Negative values are errors, positive values are used for special but
+    # normal events.
 
-    def closed? = @closed
-  end
+    Z_OK                    = 0
+    Z_STREAM_END            = 1
+    Z_NEED_DICT             = 2
+    Z_ERRNO                 = -1
+    Z_STREAM_ERROR          = -2
+    Z_DATA_ERROR            = -3
+    Z_MEM_ERROR             = -4
+    Z_BUF_ERROR             = -5
+    Z_VERSION_ERROR         = -6
 
-  class Deflate < ZStream
-    __bind_method__ :initialize, :Zlib_deflate_initialize
-    __bind_method__ :<<, :Zlib_deflate_append, 1
-    __bind_method__ :deflate, :Zlib_deflate_deflate
-    __bind_method__ :set_dictionary, :Zlib_deflate_set_dictionary, 1
-    __bind_method__ :params, :Zlib_deflate_params, 2
-    __bind_method__ :finish, :Zlib_deflate_finish, 0
-    __bind_method__ :close, :Zlib_deflate_close, 0
+    # Compression levels.
 
-    class << self
-      def deflate(input, level = -1)
-        zstream = Zlib::Deflate.new(level)
-        zstream << input
-        zstream.finish.tap do
-          zstream.close
-        end
-      end
+    Z_NO_COMPRESSION        = 0
+    Z_BEST_SPEED            = 1
+    Z_BEST_COMPRESSION      = 9
+    Z_DEFAULT_COMPRESSION   = -1
 
-      def _error(num)
-        Zlib._error(num)
-      end
-    end
-  end
+    # Compression strategies.
 
-  class Inflate < ZStream
-    __bind_method__ :initialize, :Zlib_inflate_initialize
-    __bind_method__ :<<, :Zlib_inflate_append, 1
-    __bind_method__ :inflate, :Zlib_inflate_inflate
-    __bind_method__ :finish, :Zlib_inflate_finish, 0
-    __bind_method__ :close, :Zlib_inflate_close, 0
+    Z_FILTERED              = 1
+    Z_HUFFMAN_ONLY          = 2
+    Z_RLE                   = 3
+    Z_FIXED                 = 4
+    Z_DEFAULT_STRATEGY      = 0
 
-    class << self
-      def inflate(input)
-        zstream = Zlib::Inflate.new
-        zstream << input
-        zstream.finish.tap do
-          zstream.close
-        end
-      end
+    # Possible values of the data_type field.
 
-      def _error(num)
-        Zlib._error(num)
-      end
-    end
-  end
+    Z_BINARY                = 0
+    Z_TEXT                  = 1
+    Z_UNKNOWN               = 2
 
-  class GzipFile
-    class Error < Zlib::Error; end
+    # The deflate compression method (the only one supported).
 
-    attr_writer :comment, :orig_name
+    Z_DEFLATED              = 8
 
-    def initialize(io)
-      @io = io
-      @closed = false
+    # -------------------------------------------------------------------------
+
+    def self.deflateInit(zstream, level)
+        deflateInit_(zstream, level, ZLIB_VERSION, zstream.size)
     end
 
-    def close
-      @closed = true
+    def self.deflateInit2(zstream, level, method, window_bits, mem_level, strategy)
+        deflateInit2_(zstream, level, method, window_bits, mem_level, strategy, ZLIB_VERSION, zstream.size)
     end
 
-    def closed?
-      @closed
+    attach_function :deflateInit_, [:pointer, :int, :string, :int], :int
+    attach_function :deflateInit2_, [:pointer, :int, :int, :int, :int, :int, :string, :int], :int
+    attach_function :deflate, [:pointer, :int], :int
+    attach_function :deflateEnd, [:pointer], :int
+
+    attach_function :deflateSetDictionary, [:pointer, :pointer, :uint], :int
+    attach_function :deflateCopy, [:pointer, :pointer], :int
+    attach_function :deflateReset, [:pointer], :int
+    attach_function :deflateParams, [:pointer, :int, :int], :int
+    attach_function :deflateTune, [:pointer, :int, :int, :int, :int], :int
+    attach_function :deflateBound, [:pointer, :ulong], :ulong
+    attach_function :deflatePrime, [:pointer, :int, :int], :int
+    attach_function :deflateSetHeader, [:pointer, :pointer], :int
+
+    # -------------------------------------------------------------------------
+
+    def self.inflateInit(zstream)
+        inflateInit_(zstream, ZLIB_VERSION, zstream.size)
     end
 
-    def comment
-      raise GzipFile::Error, 'closed gzip stream' if @closed
-
-      @comment
+    def self.inflateInit2(zstream, window_bits)
+        inflateInit2_(zstream, window_bits, ZLIB_VERSION, zstream.size)
     end
 
-    def orig_name
-      raise GzipFile::Error, 'closed gzip stream' if @closed
+    attach_function :inflateInit_, [:pointer, :string, :int], :int
+    attach_function :inflateInit2_, [:pointer, :int, :string, :int], :int
+    attach_function :inflate, [:pointer, :int], :int
+    attach_function :inflateEnd, [:pointer], :int
 
-      @orig_name
-    end
-  end
+    attach_function :inflateSetDictionary, [:pointer, :pointer, :uint], :int
+    attach_function :inflateSync, [:pointer], :int
+    attach_function :inflateCopy, [:pointer, :pointer], :int
+    attach_function :inflateReset, [:pointer], :int
+    attach_function :inflatePrime, [:pointer, :int, :int], :int
+    attach_function :inflateGetHeader, [:pointer, :pointer], :int
 
-  class GzipWriter < GzipFile
-    class << self
-      def wrap(io)
-        gzip_writer = new(io)
-        yield(gzip_writer)
-      ensure
-        gzip_writer.close
-      end
-    end
-  end
+    callback :in_func, [:pointer, :pointer], :uint
+    callback :out_func, [:pointer, :pointer, :uint], :int
+
+    attach_function :inflateBackInit_, [:pointer, :int, :pointer], :int
+    attach_function :inflateBack, [:pointer, :in_func, :pointer, :out_func, :pointer], :int
+    attach_function :inflateBackEnd, [:pointer], :int
+
+    # -------------------------------------------------------------------------
+
+    attach_function :compress, [:pointer, :buffer_inout, :pointer, :ulong], :int
+    attach_function :compress2, [:pointer, :pointer, :pointer, :ulong, :int], :int
+    attach_function :compressBound, [:ulong], :ulong
+
+    attach_function :uncompress, [:pointer, :buffer_inout, :pointer, :ulong], :int
+
+    # -------------------------------------------------------------------------
+
+    attach_function :gzopen, [:string, :string], :pointer
+    attach_function :gzdopen, [:int, :string], :pointer
+    attach_function :gzsetparams, [:pointer, :int, :int], :int
+    attach_function :gzread, [:pointer, :pointer, :uint], :int
+    attach_function :gzwrite, [:pointer, :pointer, :uint], :int
+    attach_function :gzprintf, [:pointer, :string, :varargs], :int
+    attach_function :gzputs, [:pointer, :string], :int
+    attach_function :gzgets, [:pointer, :pointer, :int], :pointer
+    attach_function :gzputc, [:pointer, :int], :int
+    attach_function :gzgetc, [:pointer, :pointer], :int
+    attach_function :gzungetc, [:int, :pointer], :int
+    attach_function :gzflush, [:pointer, :int], :int
+    attach_function :gzseek, [:pointer, :long, :int], :long
+    attach_function :gzrewind, [:pointer], :int
+    attach_function :gztell, [:pointer], :long
+    attach_function :gzeof, [:pointer], :int
+    attach_function :gzdirect, [:pointer], :int
+    attach_function :gzclose, [:pointer], :int
+    attach_function :gzerror, [:pointer, :buffer_out], :string
+    attach_function :gzclearerr, [:pointer], :void
+
+    # -------------------------------------------------------------------------
+
+    attach_function :adler32, [:ulong, :pointer, :uint], :ulong
+    attach_function :adler32_combine, [:ulong, :ulong, :long], :ulong
+    attach_function :crc32, [:ulong, :pointer, :uint], :ulong
+    attach_function :crc32_combine, [:ulong, :ulong, :long], :ulong
+
+    # -------------------------------------------------------------------------
+
+    attach_function :zlibCompileFlags, [ ], :ulong
+    attach_function :zError, [:int], :string
+    attach_function :inflateSyncPoint, [:pointer], :int
+    attach_function :get_crc_table, [ ], :pointer
+
+    # -------------------------------------------------------------------------
 end
+
+# =============================================================================
